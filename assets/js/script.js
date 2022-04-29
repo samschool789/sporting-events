3d0661209248b42e95735946a1ffccaaa3bf48de
CHalseth <Corey.Halseth@gmail.com>
2022-04-28 19:06:32 -0500

    api events pulled

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,33 @@
-const concertApi = "https://app.ticketmaster.com/discovery/v1/events.json?apikey=WnWHht4MA16AjiAHUluHIRgWaK2QymjF";
-console.log(concertApi);
+var locationbutton = document.getElementById('location-button');
+var datebutton = document.getElementById('insert-date');
+
+
+function getTMData() {
+    var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=US&apikey=rRT5qi1DbciQBObUX3LdsQWbrMfyiroH'
+    fetch(requestUrl)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            console.log(data._embedded);
+            var html = data._embedded.events
+                .map(user => {
+                    return `
+                    <div class="user">
+                    <p><img src=${user.images.url}/></p>
+                    <p>Name: ${user.name}</p>
+                    <p>Venue: ${user._embedded.venues.name}</p>
+                    
+                    </div>
+                    `;
+            })
+            .join("");
+            console.log(html)
+            document.querySelector('#app').innerHTML = (html)
+            })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+    
+locationbutton.addEventListener('click', getTMData);
